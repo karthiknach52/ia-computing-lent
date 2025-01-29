cd53856aeea847420bc54954cb9945ad4172491d
Karthik Nachiappan <60850111+karthiknach52@users.noreply.github.com>
2025-01-29 19:25:58 +0000

    Tests for 1C

--- Task1C.py
+++ Task1C.py
@@ -1,8 +1,7 @@
 from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
-
-def run():
+if __name__ == "__main__":
     stations = build_station_list()
     coord = (52.2053, 0.1218)
     radius = 10
@@ -12,6 +11,3 @@ def run():
     station_names.sort()
 
     print(station_names)
-
-
-run()
---
+++ test_geo.py
@@ -0,0 +1,17 @@
+from haversine import haversine
+import floodsystem.geo as geo
+from floodsystem.stationdata import build_station_list
+
+
+def test_stations_within_radius():
+    stations = build_station_list()
+    coord = (52.2053, 0.1218)
+    radius = 10
+
+    stations_in_radius = geo.stations_within_radius(stations, coord, radius)
+
+    for station in stations_in_radius:
+        if haversine(station.coord, coord) > radius:
+            assert False
+
+    assert True
