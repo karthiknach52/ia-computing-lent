4026714dd31d731e3d70c19b8b5672ab562caedd
Karthik Nachiappan <60850111+karthiknach52@users.noreply.github.com>
2025-02-07 01:59:47 +0000

    Tests for 1F

--- test_geo.py
+++ test_geo.py
@@ -1,25 +1,21 @@
 import floodsystem.geo as geo
 from floodsystem.station import MonitoringStation
 
-# Checks for stations_by_distance()
-# NEED TO IMPLEMENT A CHECK TO TEST THAT THE LIST IS SORTED BY DISTANCE
-
 
 def test_stations_by_distance_empty_input():
+    # DOCSTRING
     assert geo.stations_by_distance([], (0, 0)) == []
     # if p != (float, float):
     #     raise ValueError("p should be a tuple of floats")
 
 
-# Checks for stations_within_radius()
-
-
-def test_check_stations_are_within_radius():
+def test_check_stations_in_radius():
+    # Is this check necessary? Just repeating the function itself
     '''Check that the distance functionality works correctly'''
 
     # Create a list of test stations
     # The last 2 stations in the list of 10 should be > 10km from (0, 0)
-    stations = [MonitoringStation("s_id", "m_id", "label", (0, i * 0.01), (-2.3, 3.4445), "river", "town")
+    stations = [MonitoringStation("s_id", "m_id", "label", (0, i * 0.01), (-1, 1), "river", "town")
                 for i in range(1, 11)]
 
     stations_in_radius = geo.stations_within_radius(stations, (0, 0), 10)
@@ -27,7 +23,7 @@ def test_check_stations_are_within_radius():
     assert len(stations_in_radius) == 8
 
 
-def test_check_no_stations_in_radius_empty_input():
+def test_check_stations_in_radius_empty_input():
     '''Check that an empty list returns an empty list'''
     assert geo.stations_within_radius([], (0, 0), 10) == []
 
@@ -45,7 +41,7 @@ def test_rivers_by_station_number_empty_input():
 def test_rivers_by_station_number_most_stations():
     '''Check that the first river has the most stations'''
     # Create a list of test 15 stations which are on 5 different rivers
-    stations = [MonitoringStation("s_id", "m_id", "label", (0, 0), (-2.3, 3.4445), "river" + str(i), "town")
+    stations = [MonitoringStation("s_id", "m_id", "label", (0, 0), (-1, 1), "river" + str(i), "town")
                 for i in [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]]
 
     assert geo.rivers_by_station_number(stations, 3)[0][1] == 5
@@ -54,7 +50,7 @@ def test_rivers_by_station_number_most_stations():
 def test_rivers_by_station_number_length_N():
     '''Check that only N rivers are returned (edge case tested below)'''
     # Create a list of test 15 stations which are on 5 different rivers
-    stations = [MonitoringStation("s_id", "m_id", "label", (0, 0), (-2.3, 3.4445), "river" + str(i), "town")
+    stations = [MonitoringStation("s_id", "m_id", "label", (0, 0), (-1, 1), "river" + str(i), "town")
                 for i in [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]]
 
     assert len(geo.rivers_by_station_number(stations, 3)) == 3
@@ -63,7 +59,7 @@ def test_rivers_by_station_number_length_N():
 def test_rivers_by_station_number_correct_length():
     '''Check that rivers with the same number of stations as the Nth river are included'''
     # Create a list of test 10 stations which are on 5 different rivers
-    stations = [MonitoringStation("s_id", "m_id", "label", (0, 0), (-2.3, 3.4445), "river" + str(i % 5), "town")
+    stations = [MonitoringStation("s_id", "m_id", "label", (0, 0), (-1, 1), "river" + str(i % 5), "town")
                 for i in range(1, 11)]
 
     assert len(geo.rivers_by_station_number(stations, 3)) == 5
--- test_station.py
+++ test_station.py
@@ -4,6 +4,7 @@
 """Unit test for the station module"""
 
 from floodsystem.station import MonitoringStation
+from floodsystem.station import inconsistent_typical_range_stations
 
 
 def test_create_monitoring_station():
@@ -26,6 +27,19 @@ def test_create_monitoring_station():
     assert s.river == river
     assert s.town == town
 
-# NEED TO IMPLEMENT TESTS FOR:
-# typical_range_consistent()
-# inconsistent_typical_range_station()
+
+def test_no_typical_range():
+    '''Check that a missing typical range leads to marking as inconsistent'''
+    # Station where the typical range is None
+    assert not MonitoringStation("s_id", "m_id", "label", (0, 0), None, "river", "town").typical_range_consistent()
+
+
+def test_nonsense_typical_range():
+    '''Check that a nonsense typical range leads to marking as inconsistent'''
+    # Station where the typical range high is lower than the low
+    assert not MonitoringStation("s_id", "m_id", "label", (0, 0), (1, -1), "river", "town").typical_range_consistent()
+
+
+def test_inconsistent_typical_range_empty_input():
+    '''Check that an empty list returns an empty list'''
+    assert inconsistent_typical_range_stations([]) == []
