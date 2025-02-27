c2a6c4413d67676489ab933bcf43c724d23bc4b9
Ryan <rw786@cam.ac.uk>
2025-02-27 20:52:35 +0000

    Added a few docstrings

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def polyfit(dates, levels, p):
+    '''Find the least square fit polynomial to simulate the plot_water_level graph'''
     x = date2num(dates)
     p_coeff = np.polyfit(x - x[0], levels, p)
     poly = np.poly1d(p_coeff)
