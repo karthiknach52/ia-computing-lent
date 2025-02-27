0937dfc814243b279f3db6e87a3d7fa5180b5110
Karthik Nachiappan <60850111+karthiknach52@users.noreply.github.com>
2025-02-27 22:26:35 +0000

    Try to fix polyfit test

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -4,6 +4,8 @@ import numpy as np
 
 def polyfit(dates, levels, p):
     '''Find the least square fit polynomial to simulate the plot_water_level graph'''
+    if len(dates) != len(levels):
+        raise ValueError("Dates and levels are different lengths")
     x = date2num(dates)
     p_coeff = np.polyfit(x - x[0], levels, p)
     poly = np.poly1d(p_coeff)
--- test_analysis.py
+++ test_analysis.py
@@ -2,10 +2,14 @@ import floodsystem.analysis as analysis
 
 
 def test_polyfit():
-    '''Test that polyfit returns a valid polynomial'''
-    a = [i for i in range(5)]
-    poly, d = analysis.polyfit(a, a, 2)
-    if poly(0) == float(0):
+    '''Test that no polynomial is returned when dates and levels are not the same length'''
+    dates = [i for i in range(9)]
+    levels = [i for i in range(10)]
+
+    try:
+        analysis.polyfit(dates, levels, 2)
+    except ValueError:
         assert True
         return
+
     assert False
