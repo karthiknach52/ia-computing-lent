37abdb348010552153e4f3f782a83713d6328dc3
Karthik Nachiappan <60850111+karthiknach52@users.noreply.github.com>
2025-02-27 22:19:48 +0000

    Try to fix test failing on workflow

--- test_analysis.py
+++ test_analysis.py
@@ -2,10 +2,10 @@ import floodsystem.analysis as analysis
 
 
 def test_polyfit():
-    '''Test that polyfit returns a polynomial'''
+    '''Test that polyfit returns a valid polynomial'''
     a = [i for i in range(5)]
     poly, d = analysis.polyfit(a, a, 2)
-    if poly(0) != 0:
-        assert False
+    if poly(0) == float(0):
+        assert True
         return
-    assert True
+    assert False
