d1603f9ce6657d2509c089402cf8aee50f5b0b90
Karthik Nachiappan <60850111+karthiknach52@users.noreply.github.com>
2025-02-27 22:12:42 +0000

    Add timeout to workflow

--- test_analysis.py
+++ test_analysis.py
@@ -5,7 +5,7 @@ def test_polyfit():
     '''Test that polyfit returns a polynomial'''
     a = [i for i in range(5)]
     poly, d = analysis.polyfit(a, a, 2)
-    if poly(0) == 0:
-        assert True
+    if poly(0) != 0:
+        assert False
         return
-    assert False
+    assert True
